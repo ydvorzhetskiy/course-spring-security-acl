7452750b7c736bf976e3d4ee79bfbf225379d2b9
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2021-05-20 10:10:51 +0600

    Initial

---
+++ src/main/java/edu/spingsecurity/model/User.java
@@ -0,0 +1,36 @@
+package edu.spingsecurity.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.List;
+
+@SequenceGenerator(
+    name = "user_seq_gen",
+    sequenceName = "user_seq",
+    initialValue = 2010
+)
+@Entity
+@Data
+public class User {
+
+    @EqualsAndHashCode.Include
+    @GeneratedValue(generator = "user_seq_gen")
+    @Id
+    private long id;
+
+    private String login;
+
+    private String password;
+
+    @ToString.Exclude
+    @ElementCollection
+    private List<String> roles;
+
+    @ToString.Exclude
+    @JoinTable(name = "user_company")
+    @ManyToMany(cascade = CascadeType.ALL)
+    private List<Company> companies;
+}
